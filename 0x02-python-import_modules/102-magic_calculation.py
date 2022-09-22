8b5a5b9836b9c1704ad2e73717469934f4d6fd45
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-09-22 22:02:00 +0300

    magic cal

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b))
