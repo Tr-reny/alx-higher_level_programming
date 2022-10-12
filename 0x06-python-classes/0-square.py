a5603f06b6158db5aee2e8bc6d88f5104d733283
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-10-12 21:07:38 +0300

    square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Empty class Square that defines a square"""
+
+
+class Square:
+    """Empty class Square that defines a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square that defines a square"""
+    def __init__(self, size):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
