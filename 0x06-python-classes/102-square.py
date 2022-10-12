5b5365d48cf3f3efec304e371260e580b691fd8b
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-10-12 21:16:20 +0300

    102 square

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square that defines a square"""
+    def __init__(self, size=0):
+        """ init square
+
+        Args:
+            value (int): size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """int: private size.
+
+        Returns:
+            Private size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int.
+
+        Args:
+            value (int): size of the square.
+        """
+        if type(value) is not int and type(value) is not float:
+            raise TypeError('size must be a number')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value  #: size of the square
+
+    def area(self):
+        """returns the area
+
+        Returns:
+            area.
+        """
+        return self.__size**2
+
+    def __lt__(self, other):
+        return self.size < other.size
+
+    def __le__(self, other):
+        return self.size <= other.size
+
+    def __eq__(self, other):
+        return self.size == other.size
+
+    def __ne__(self, other):
+        return self.size != other.size
+
+    def __ge__(self, other):
+        return self.size >= other.size
