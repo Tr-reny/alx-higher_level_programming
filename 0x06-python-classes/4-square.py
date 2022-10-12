19d72bd5dd9c229e023498d63e54dba15b872066
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-10-12 21:12:11 +0300

    4 square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square that defines a square"""
+    def __init__(self, size=0):
+        """ init square
+
+        Args:
+            value (int): size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """int: private size.
+
+        Returns:
+            Private size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int.
+
+        Args:
+            value (int): size of the square.
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value  #: size of the square
+
+    def area(self):
+        """returns the area
+
+        Returns:
+            area.
+        """
+        return self.__size**2
