2edd793a0df240d3e59f64210f40dca65ab79899
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-10-17 20:39:06 +0300

    rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ class rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ class rectangle"""
+    def __init__(self, width=0, height=0):
+        """ Instantiation with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ width setter
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ height setter
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
