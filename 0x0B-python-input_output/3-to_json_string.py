71ad1c4a9bc809ba353c208f39c2b5a0a6ee30fc
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-10-25 21:41:13 +0300

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that contains a function that returns the JSON
+representation of an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
+
+    Args:
+        my_obj: object
+
+    Raises:
+        Exception: when the object can't be encoded
+
+    """
+    return json.dumps(my_obj)
