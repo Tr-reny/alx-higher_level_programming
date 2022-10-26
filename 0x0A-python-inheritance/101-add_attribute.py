3a535d233517a957dfe6760433f3844ea374c98d
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-10-26 19:23:14 +0300

    add attribs

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""dds a new attribute to an object
+if it’s possible
+"""
+
+
+def add_attribute(obj, attribute, value):
+    """dds a new attribute"""
+    if '__dict__' not in dir(obj):
+        raise TypeError("can't add new attribute")
+    if '__slots__' in dir(obj):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attribute, value)
