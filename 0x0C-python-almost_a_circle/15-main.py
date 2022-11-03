a51d14e6d779703f3485cf665a163b73a05397e7
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-11-03 11:04:22 +0300

    15

---
+++ 0x0C-python-almost_a_circle/15-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 15-main """
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 7, 2, 8)
+    r2 = Rectangle(2, 4)
+    Rectangle.save_to_file([r1, r2])
+
+    with open("Rectangle.json", "r") as file:
+        print(file.read())
