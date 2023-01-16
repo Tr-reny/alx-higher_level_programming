804056acd01f55c4a7d7a4b731382250c6db652e
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-01-16 12:27:02 -0500

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# This script takes in a URL as its first argument and uses curl to send a request to that URL with the "-sI" options (silent, head only).
-# The response headers are piped to grep, which searches for the "Content-Length" field, which gives the size of the body in bytes.
-# This value is then printed using awk.
-
+# Display the size of the body of a response from curling a URL
 curl -sI "$1" | grep 'Content-Length' | cut -d' ' -f2
