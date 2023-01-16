4f0a9c58c4a76272f6c98fa45785ae46096c87cd
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-01-16 13:04:55 -0500

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request using curl
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
